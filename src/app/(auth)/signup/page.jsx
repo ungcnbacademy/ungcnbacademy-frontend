f9c4c43a3855d9ff11d5068430b43b3850d8755f
Shafin <100669577+shafin003@users.noreply.github.com>
2025-01-16 14:48:14 +0600

    signup design fixed

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -4,19 +4,19 @@ import Link from 'next/link';
 import styles from './page.module.css';
 import useAxios from '@/hooks/useAxios';
 import { configuration } from '@/configuration/configuration';
-import { Modal, Result } from 'antd';
+
 import { useRouter } from 'next/navigation';
 import Button from '@/components/ui/button/button';
 import Input from '../../../components/ui/input/input';
 import Message from '../../../components/ui/message/message';
+import Image from 'next/image';
 export default function Signup() {
 	const [data, setData] = useState({});
 	const [btnDisabled, setBtnDisabled] = useState(true);
 	const [confirmPassword, setConfirmPassword] = useState('');
 	const [response, error, loading, axiosFetch] = useAxios();
-	const router = useRouter();
-	const [isModalOpen, setIsModalOpen] = useState(false);
 	const [message, setMessage] = useState('');
+
 	function handleChange(event) {
 		const { name, value } = event.target;
 		setData({ ...data, [name]: value });
@@ -60,98 +60,101 @@ export default function Signup() {
 
 	return (
 		<div className={styles.main}>
-			<div className={styles.container}>
-				<p className={styles.title}>SIGNUP</p>
-				<br />
-				<form
-					onSubmit={onSignupSubmitHandlerAsync}
-					className={styles.form}
-				>
-					<Input
-						type="text"
-						placeholder="Enter your username"
-						name="name"
-						onChange={handleChange}
-						value={data?.name || ''}
-						required
-					/>
-					<Input
-						type="email"
-						placeholder="Enter your email"
-						name="email"
-						onChange={handleChange}
-						value={data?.email || ''}
-						required
-					/>
-					<Input
-						type="tel"
-						name="phone"
-						placeholder="Enter your phone"
-						onChange={handleChange}
-						value={data?.phone || ''}
-					/>
-					<Input
-						type="password"
-						placeholder="Enter your password"
-						name="password"
-						onChange={handleChange}
-						value={data?.password || ''}
-						required
-					/>
-					<Input
-						type="password"
-						placeholder="Confirm your password"
-						name="password"
-						onChange={(e) => {
-							setConfirmPassword(e.target.value);
-							setBtnDisabled(e.target.value !== data?.password);
-						}}
-						value={confirmPassword}
-						required
-					/>
-					<Message text={message} type="error" />
-					<br /> <br />
-					<Button
-						type="submit"
-						text="signup"
-						variant="primary"
-						disabled={btnDisabled}
-						loading={loading}
-					/>
-				</form>
-
-				<small className={styles.small}>
-					Already have an account?{' '}
-					<Link href="/login/client" className={styles.link}>
-						Click here
-					</Link>
-				</small>
-			</div>
-			<Modal open={isModalOpen} footer={null} closable={false}>
-				<Result
-					status="success"
-					title="Signup Successful"
-					subTitle="Please login to your account to continue"
-					extra={[
-						<Button
-							key="login"
-							text="Login"
-							variant="primary"
-							onClick={() => {
-								router.push('/login/client');
-							}}
-						/>,
-						<Button
-							key="home"
-							text="Home"
-							variant="secondary"
-							onClick={() => {
-								router.push('/');
-							}}
-						/>,
-					]}
+			<Link href="/">
+				<Image
+					src={'/logoBlack.svg'}
+					alt="Logo"
+					width={160}
+					height={50}
 				/>
-			</Modal>
+			</Link>
+			<form onSubmit={onSignupSubmitHandlerAsync} className={styles.form}>
+				<label className={styles.label}>First Name</label>
+				<Input
+					type="text"
+					placeholder="Enter your first name"
+					name="firstName"
+					onChange={handleChange}
+					value={data?.firstName || ''}
+					variant="outLined"
+					required
+					className={styles.input}
+				/>
+				<label className={styles.label}>Last Name</label>
+				<Input
+					type="text"
+					placeholder="Enter your last name"
+					name="lastName"
+					onChange={handleChange}
+					value={data?.lastName || ''}
+					variant="outLined"
+					required
+					className={styles.input}
+				/>
+				<label className={styles.label}>Email</label>
+				<Input
+					type="email"
+					placeholder="Enter your email"
+					name="email"
+					onChange={handleChange}
+					value={data?.email || ''}
+					variant="outLined"
+					required
+					className={styles.input}
+				/>
+				<label className={styles.label}>Password</label>
+				<Input
+					type="password"
+					placeholder="Enter your password"
+					name="password"
+					onChange={handleChange}
+					variant="outLined"
+					value={data?.password || ''}
+					required
+					className={styles.input}
+				/>
+				<label className={styles.label}>Confirm password</label>
+				<Input
+					type="password"
+					placeholder="Confirm your password"
+					name="password"
+					onChange={(e) => {
+						setConfirmPassword(e.target.value);
+						setBtnDisabled(e.target.value !== data?.password);
+					}}
+					variant="outLined"
+					value={confirmPassword}
+					required
+					className={styles.input}
+				/>
+				<Message text={message} type="error" />
+
+				<Button
+					type="submit"
+					text="Sign up"
+					variant="outLined"
+					disabled={btnDisabled}
+					loading={loading}
+					className={styles.button}
+				/>
+			</form>
+			{error?.message && <Message text={error?.message} type="error" />}
+			<small className={styles.or}>or</small>
+			<div className={styles.googleButton}>
+				<Image
+					src="/icons/google.svg"
+					alt="Google"
+					width={20}
+					height={20}
+				/>
+				<p className={styles.googleText}>Sign up with google</p>
+			</div>
+			<small className={styles.small}>
+				Already have an account?{' '}
+				<Link href="/login" className={styles.link}>
+					Login
+				</Link>
+			</small>
 		</div>
 	);
 }
