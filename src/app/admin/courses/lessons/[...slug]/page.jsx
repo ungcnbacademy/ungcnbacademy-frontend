31a3caecdcaff2a74385be2993dfa9be93dc243a
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-02 00:03:18 +0600

    bug fixes

--- src/app/admin/courses/lessons/[...slug]/page.jsx
+++ src/app/admin/courses/lessons/[...slug]/page.jsx
@@ -54,67 +54,91 @@ export default function LessonDetails({ params }) {
 		});
 	};
 
-	const StreamVideo = () => {
+	const lessonDetailsRender = () => {
 		return (
-			<div className={styles.videoContainer}>
-				<Stream
-					src={response?.data?.cloudflareVideoId}
-					controls
-					className={styles.video}
-				/>
+			<>
+				<small>Lesson Details</small>
+				<h2 className={styles.title}>{response?.data?.title}</h2>
+				<p className={styles.subtitle}># {response?.data?._id}</p>
+				<p className={styles.subtitle}>
+					Duration: {response?.data?.duration}{' '}
+					{Number(response?.data?.durationUnit) > 1
+						? 'minutes'
+						: 'seconds'}{' '}
+				</p>
+				<p className={styles.subtitle}>
+					Description: {response?.data?.description}
+				</p>
+				<p className={styles.subtitle}>
+					Created at:{' '}
+					{moment(response?.data?.createdAt).format('lll')}
+				</p>
+				<br />
+			</>
+		);
+	};
+
+	const streamVideoRender = () => {
+		return (
+			<>
+				{response?.data?.cloudflareVideoId && (
+					<div className={styles.videoContainer}>
+						<Stream
+							src={response?.data?.cloudflareVideoId}
+							controls
+							className={styles.video}
+						/>
+					</div>
+				)}
+			</>
+		);
+	};
+
+	const uploadVideoRender = () => {
+		return (
+			<div className={styles.uploadVideo}>
+				<label className={styles.label}>Upload or change video</label>
+				<form className={styles.form} onSubmit={onVideoUploadHandler}>
+					<Input
+						type="file"
+						variant="secondary"
+						label="Upload Video"
+						placeholder="Upload Video"
+						name="video"
+						className={styles.input}
+					/>
+					<Message
+						text={message.text}
+						type={message.type}
+						loading={loadingVideoUpload}
+					/>
+					<Button
+						type="submit"
+						text="Upload"
+						variant="primary"
+						className={styles.button}
+						loading={loadingVideoUpload}
+					/>
+				</form>
 			</div>
 		);
 	};
 
 	return (
 		<div className={styles.main}>
-			<small>Lesson Details</small>
 			{loading && <LoadingDots />}
 			{response?.data && !loading && !error && (
-				<>
-					<h2 className={styles.title}>{response?.data?.title}</h2>
-					<p># {response?.data?._id}</p>
-					<p>Duration: {response?.data?.duration} </p>
-					<p>Description: {response?.data?.description}</p>
-					<p>
-						Created at:{' '}
-						{moment(response?.data?.createdAt).format('lll')}
-					</p>
-					<br />
-
-					{response?.data?.cloudflareVideoId && <StreamVideo />}
-
-					<div className={styles.uploadVideo}>
-						<label className={styles.label}>
-							Upload or change video
-						</label>
-						<form
-							className={styles.form}
-							onSubmit={onVideoUploadHandler}
-						>
-							<Input
-								type="file"
-								variant="secondary"
-								label="Upload Video"
-								placeholder="Upload Video"
-								name="video"
-								className={styles.input}
-							/>
-							<Message
-								text={message.text}
-								type={message.type}
-								loading={loadingVideoUpload}
-							/>
-							<Button
-								type="submit"
-								text="Upload"
-								variant="primary"
-								className={styles.button}
-								loading={loadingVideoUpload}
-							/>
-						</form>
+				<div className={styles.container}>
+					<div className={styles.left}>
+						{lessonDetailsRender()}
+						{streamVideoRender()}
+						{uploadVideoRender()}
 					</div>
-				</>
+					<div className={styles.right}>
+						<small>Lesson Details</small>
+						<h2 className={styles.title}>Assets</h2>
+					</div>
+				</div>
 			)}
 		</div>
 	);
