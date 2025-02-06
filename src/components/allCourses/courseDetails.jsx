182a6cf4d96fd272ef630ddc0877a2bc78caca54
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-06 10:04:32 +0600

    bug fixes

--- src/components/allCourses/courseDetails.jsx
+++ src/components/allCourses/courseDetails.jsx
@@ -6,6 +6,7 @@ import { getAmountsWithCommas } from '@/utils/utils';
 import moment from 'moment';
 import Button from '../ui/button/button';
 import Collapse from '../ui/collapse/collapse';
+import Avatar from '../ui/avatar/avatar';
 export default async function CourseDetails({ id }) {
 	const response = await getFetchRequests.getCourseById(id);
 	console.log(response);
@@ -24,7 +25,15 @@ export default async function CourseDetails({ id }) {
 					<p className={styles.description}>
 						{response?.data?.description}
 					</p>
+					<br />
 					<div>
+						<h3 className={styles.title}>Course content</h3>
+						<p className={styles.subtitle}>
+							{response?.data?.statistics?.totalModules} Modules, {' '}
+							{ response?.data?.statistics?.totalLessons} Lessons, {' '}
+							{response?.data?.statistics?.totalDuration.toFixed(2)} minutes, {' '}
+							{response?.data?.statistics?.totalQuizzes} Quizzes
+						</p>
 						{response?.data?.modules?.length > 0 &&
 							response?.data?.modules?.map((module, i) => (
 								<Collapse
@@ -33,26 +42,51 @@ export default async function CourseDetails({ id }) {
 										{
 											title: `Module ${module.order}: ${module.title}`,
 											description: module.description,
-											children: module?.lessons?.length > 0 && module?.lessons?.map(
-												(lesson, i) => (
-													<Collapse
-														key={i}
-														variant="secondary"
-														data={[
-															{
-																title: `Lesson ${lesson.order}: ${lesson.title}`,
-																description:
-																	lesson.description,
-																children: null,
-															},
-														]}
-													/>
-												)
-											),
+											children:
+												module?.lessons?.length > 0 &&
+												module?.lessons?.map(
+													(lesson, i) => (
+														<Collapse
+															key={i}
+															variant="secondary"
+															data={[
+																{
+																	title: `Lesson ${lesson.order}: ${lesson.title}`,
+																	description:
+																		lesson.description,
+																	children:
+																		null,
+																},
+															]}
+														/>
+													)
+												),
 										},
 									]}
 								/>
 							))}
+					</div>
+					<br />
+					<div>
+						<h3 className={styles.title}>Instructor</h3>
+						{response?.data?.instructors && (
+							response?.data?.instructors?.map((instructor, i) => (
+								<div key={i} className={styles.instructor}>
+									<Avatar
+										icon
+										image={instructor?.image}
+									/>
+									<p	className={styles.name} > {instructor?.name} </p>
+									<p> {instructor?.designation} </p>
+									<p> {instructor?.bio} </p>
+									<div className={styles.socialLinks}>
+										{instructor?.socialLinks?.linkedin && <a className={styles.socialLink} href={instructor?.socialLinks?.linkedin} target="_blank" >{instructor?.socialLinks?.linkedin}</a>}
+										{instructor?.socialLinks?.twitter && <a className={styles.socialLink} href={instructor?.socialLinks?.twitter} target="_blank" >{instructor?.socialLinks?.twitter}</a>}
+										{instructor?.socialLinks?.website && <a className={styles.socialLink} href={instructor?.socialLinks?.website} target="_blank" >{instructor?.socialLinks?.website}</a>}
+									</div>
+								</div>
+							))
+						)}
 					</div>
 				</div>
 				<div className={styles.right}>
