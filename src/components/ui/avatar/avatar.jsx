0afa0578bc77c5cd43b691e40f31528f02cf9605
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-06 10:12:14 +0600

    bug fixes

--- src/components/ui/avatar/avatar.jsx
+++ src/components/ui/avatar/avatar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './avatar.module.css';
 import Image from 'next/image';
 import { HiUser } from 'react-icons/hi2';
-export default function Avatar({ name, image, icon, size = 28 }) {
+export default function Avatar({ name, image, icon, size = 32 }) {
 	return (
 		<div className={styles.main}>
 			{image && (
