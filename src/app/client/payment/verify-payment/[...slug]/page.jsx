aed2f11c8c071018bdc34e827e945d93c7a66744
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-15 19:13:56 +0600

    bug fixes

--- src/app/client/payment/verify-payment/[...slug]/page.jsx
+++ src/app/client/payment/verify-payment/[...slug]/page.jsx
@@ -1,10 +1,14 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
+import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Status() {
     const [slugs, setSlugs] = useState('');
+const searchParams = useSearchParams();
+const pathname = usePathname();
 
+const tranId = searchParams.get('tran_id');
     useEffect(() => {
         const url = new URL(window.location.href);
         const slugs = url.pathname.split('/').pop();
@@ -14,10 +18,12 @@ export default function Status() {
     return (
         <div className={styles.overlay}>
             <div className={styles.header}>
-                <h3 className={styles.title}>Payment status normal</h3>
+                <h3 className={styles.title}>Payment status dynamic</h3>
 
             </div>
 						<h4>{slugs}</h4> {/* Now this will work */}
+						<h4>{tranId}</h4>
+						<h4>{pathname}</h4>
         </div>
     );
 }
