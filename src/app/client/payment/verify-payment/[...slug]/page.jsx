4317ac95fce284646cbecaa6963be38abd772150
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-16 05:33:17 +0600

    bug fixes

--- src/app/client/payment/verify-payment/[...slug]/page.jsx
+++ src/app/client/payment/verify-payment/[...slug]/page.jsx
@@ -4,26 +4,25 @@ import styles from './page.module.css';
 import { usePathname, useSearchParams } from 'next/navigation';
 
 export default function Status() {
-    const [slugs, setSlugs] = useState('');
-const searchParams = useSearchParams();
-const pathname = usePathname();
+	const [slugs, setSlugs] = useState('');
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
 
-const tranId = searchParams.get('tran_id');
-    useEffect(() => {
-        const url = new URL(window.location.href);
-        const slugs = url.pathname.split('/').pop();
-        setSlugs(slugs); // Store the value in state
-    }, []);
+	const tranId = searchParams.get('tran_id');
+	useEffect(() => {
+		const url = new URL(window.location.href);
+		const slugs = url.pathname.split('/').pop();
+		setSlugs(slugs); // Store the value in state
+	}, []);
 
-    return (
-        <div className={styles.overlay}>
-            <div className={styles.header}>
-                <h3 className={styles.title}>Payment status dynamic</h3>
-
-            </div>
-						<h4>{slugs}</h4> {/* Now this will work */}
-						<h4>{tranId}</h4>
-						<h4>{pathname}</h4>
-        </div>
-    );
+	return (
+		<div className={styles.overlay}>
+			<div className={styles.header}>
+				<h3 className={styles.title}>Payment status dynamic</h3>
+			</div>
+			<h4>{slugs}</h4> {/* Now this will work */}
+			<h4>{tranId}</h4>
+			<h4>{pathname}</h4>
+		</div>
+	);
 }
--- src/app/client/payment/verify-payment/page.jsx
+++ src/app/client/payment/verify-payment/page.jsx
@@ -3,21 +3,20 @@ import React, { useEffect, useState } from 'react';
 import styles from './page.module.css';
 
 export default function Status() {
-    const [slugs, setSlugs] = useState('');
+	const [slugs, setSlugs] = useState('');
 
-    useEffect(() => {
-        const url = new URL(window.location.href);
-        const slugs = url.pathname.split('/').pop();
-        setSlugs(slugs); // Store the value in state
-    }, []);
+	useEffect(() => {
+		const url = new URL(window.location.href);
+		const slugs = url.pathname.split('/').pop();
+		setSlugs(slugs); // Store the value in state
+	}, []);
 
-    return (
-        <div className={styles.overlay}>
-            <div className={styles.header}>
-                <h3 className={styles.title}>Payment status normal</h3>
-
-            </div>
-						<h4>{slugs}</h4> {/* Now this will work */}
-        </div>
-    );
+	return (
+		<div className={styles.overlay}>
+			<div className={styles.header}>
+				<h3 className={styles.title}>Payment status normal</h3>
+			</div>
+			<h4>{slugs}</h4> {/* Now this will work */}
+		</div>
+	);
 }
