3c521d4e99e85913867852d00f019eb3b2f5d492
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-16 06:02:08 +0600

    bug fixes

--- src/app/(no-auth)/about-us/page.jsx
+++ src/app/(no-auth)/about-us/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './page.module.css';
 
 export default function AboutUs() {
 	return <div>AboutUs</div>;
--- src/app/(no-auth)/faculties/page.jsx
+++ src/app/(no-auth)/faculties/page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styles from './page.module.css';
 
 export default function Faculties() {
 	return <div>Faculties</div>;
