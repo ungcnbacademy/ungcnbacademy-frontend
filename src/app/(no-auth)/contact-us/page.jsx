942211ee21fdb31427095641adbf7f6b97cad2d9
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-23 11:57:24 +0600

    bug fixes

--- src/app/(no-auth)/contact-us/page.jsx
+++ src/app/(no-auth)/contact-us/page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import TopSection from '@/components/contact-us/topSection';
-import InfoSection from '@/components/contact-us/infoSection';
-import MapSection from '@/components/contact-us/mapSection';
+import TopSection from '@/components/contactUs/topSection';
+import InfoSection from '@/components/contactUs/infoSection';
+import MapSection from '@/components/contactUs/mapSection';
 export default function ContactUs() {
-	return <div>
-		<TopSection/>
-		<InfoSection/>
-		<MapSection/>
-	</div>;
+  return (
+    <div>
+      <TopSection />
+      <InfoSection />
+      <MapSection />
+    </div>
+  );
 }
--- src/app/(no-auth)/courses/[id]/page.jsx
+++ src/app/(no-auth)/courses/[id]/page.jsx
@@ -2,11 +2,11 @@ import CourseDetails from '@/components/allCourses/courseDetails';
 import React from 'react';
 
 export default async function Details({ params }) {
-	const courseId = (await params).id;
+  const courseId = (await params).id;
 
-	return (
-		<div>
-			<CourseDetails id={courseId} />
-		</div>
-	);
+  return (
+    <div>
+      <CourseDetails id={courseId} />
+    </div>
+  );
 }
