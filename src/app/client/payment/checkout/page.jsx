2562fb6d2b5691ba36464fdf1f8c3ff4545bce37
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-24 18:06:31 +0600

    bug fixes

--- src/app/client/payment/checkout/page.jsx
+++ src/app/client/payment/checkout/page.jsx
@@ -12,175 +12,139 @@ import { redirect } from 'next/navigation';
 import Image from 'next/image';
 import Message from '@/components/ui/message/message';
 export default function Checkout() {
-	const { globalCart } = useContext(CartContext);
-	const [response, error, loading, axiosFetch] = useAxios();
-	const [checkMark, setCheckMark] = useState(false);
-	const [message, setMessage] = useState({ text: '', type: '' });
+  const { globalCart } = useContext(CartContext);
+  const [response, error, loading, axiosFetch] = useAxios();
+  const [checkMark, setCheckMark] = useState(false);
+  const [message, setMessage] = useState({ text: '', type: '' });
 
-	const checkoutClickHandler = (event) => {
-		event.preventDefault();
-		if (!checkMark) {
-			setMessage({
-				text: 'Please agree to our terms and conditions',
-				type: 'error',
-			});
-			return;
-		}
-		const formData = new FormData(event.target);
-		formData.delete('check');
-		const payload = Object.fromEntries(formData);
+  const checkoutClickHandler = (event) => {
+    event.preventDefault();
+    if (!checkMark) {
+      setMessage({
+        text: 'Please agree to our terms and conditions',
+        type: 'error',
+      });
+      return;
+    }
+    const formData = new FormData(event.target);
+    formData.delete('check');
+    const addressPayload = Object.fromEntries(formData);
 
-		axiosFetch({
-			method: 'POST',
-			url: `${configuration?.client?.payment}/${globalCart?.courseId}/initiate-course`,
-			requestConfig: {
-				redirectUrl: `${window.location.origin}/client/payment/verify-payment`,
-				shippingAddress: payload,
-			},
-		});
-	};
+    const url =
+      globalCart?.type === 'Full Course'
+        ? `${configuration.client.payment}/${globalCart?.courseId}/initiate-course`
+        : `${configuration.client.payment}/${globalCart?.courseId}/initiate-module`;
 
-	useEffect(() => {
-		if (error?.message) {
-			setMessage({ text: error?.message, type: 'error' });
-		}
-	}, [error]);
+    const payload =
+      globalCart?.type === 'Full Course'
+        ? {
+            shippingAddress: addressPayload,
+						redirectUrl: `${window.location.origin}/client/payment/verify-payment`,
+          }
+        : {
+						redirectUrl: `${window.location.origin}/client/payment/verify-payment`,
+            shippingAddress: addressPayload,
+            moduleIds: [globalCart?.moduleId],
+          };
 
-	useEffect(() => {
-		if (response?.data?.gatewayRedirectURL) {
-			//redirect to payment gateway
-			window.location.href = response?.data?.gatewayRedirectURL;
-		}
-	}, [response]);
+    axiosFetch({
+      method: 'POST',
+      url: url,
+      requestConfig: payload,
+    });
+  };
 
-	useEffect(() => {
-		if (!globalCart) {
-			redirect('/courses');
-		}
-	}, [globalCart]);
+  useEffect(() => {
+    if (error?.message) {
+      setMessage({ text: error?.message, type: 'error' });
+    }
+  }, [error]);
 
-	const addressFormRender = () => {
-		return (
-			<div className={styles.formContainer}>
-				<h3 className={styles.title}>Shipping Address</h3>
-				<form className={styles.form} onSubmit={checkoutClickHandler}>
-					<label>Phone (ex: 01700000000)</label>
-					<Input
-						type="text"
-						placeholder="Enter your phone"
-						variant="secondary"
-						name="phone"
-						required
-					/>
-					<label>Address</label>
-					<Input
-						type="text"
-						placeholder="Enter your address"
-						variant="secondary"
-						name="address"
-						required
-					/>
-					<label>City</label>
-					<Input
-						type="text"
-						placeholder="Enter your city"
-						variant="secondary"
-						name="city"
-						required
-					/>
-					<label>Country</label>
-					<Input
-						type="text"
-						placeholder="Enter your country"
-						variant="secondary"
-						name="country"
-						required
-					/>
-					<br />
-					<div className={styles.checkContainer}>
-						<Input
-							type="checkbox"
-							name="check"
-							className={styles.check}
-							onChange={(e) => setCheckMark(e.target.checked)}
-						/>
-						<label className={styles.checkLabel}>
-							Please agree to our terms and conditions &nbsp;
-							<Link href="/terms-of-sale" className={styles.link}>
-								Terms and condition
-							</Link>
-						</label>
-					</div>
-					<Message text={message.text} type={message.type} />
-					<Button
-						type="submit"
-						variant="secondary"
-						text="Checkout"
-						loading={loading}
-						className={styles.button}
-					/>
-				</form>
-			</div>
-		);
-	};
-	const coursePaymentInfoRender = () => {
-		return (
-			<div className={styles.infoWrapper}>
-				<h2 className={styles.title}>Course Details</h2>
-				<div className={styles.infoContainer}>
-					<h1 className={styles.title}>
-						{globalCart?.courseTitle || ''}
-					</h1>
-					{globalCart?.moduleTitle && (
-						<p className={styles.allModules}>
-							Module {globalCart?.moduleOrder}:{' '}
-							{globalCart?.moduleTitle || ''}
-						</p>
-					)}
-					{globalCart?.allModules && (
-						<div className={styles.allModules}>
-							{globalCart?.allModules?.map((module) => (
-								<p key={module?._id}>
-									Module {module?.order}:{' '}
-									{module?.title || ''}
-								</p>
-							))}
-						</div>
-					)}
-					<p className={styles.type}>
-						Type: <span>{globalCart?.type || ''}</span>{' '}
-					</p>
-					<p className={styles.price}>
-						Price:{' '}
-						{getAmountsWithCommas(
-							globalCart?.price || globalCart?.modulePrice || ''
-						)}
-					</p>
-				</div>
-				<br />
-				<h2 className={styles.title}>Payment </h2>
-				<div className={styles.paymentContainer}>
-					<p className={styles.paymentText}>Powered by</p>
-					<Image
-						src="/assets/payment/1.png"
-						alt="Empty"
-						width={180}
-						height={35}
-						className={styles.paymentIcon}
-					/>
-				</div>
-			</div>
-		);
-	};
-	return (
-		<div className={styles.overlay}>
-			<div className={styles.header}>
-				<h3 className={styles.title}>Checkout</h3>
-			</div>
-			<div className={styles.main}>
-				<div className={styles.left}>{addressFormRender()}</div>
-				<div className={styles.right}>{coursePaymentInfoRender()}</div>
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    if (response?.data?.gatewayRedirectURL) {
+      //redirect to payment gateway
+      window.location.href = response?.data?.gatewayRedirectURL;
+    }
+  }, [response]);
+
+  useEffect(() => {
+    if (!globalCart) {
+      redirect('/courses');
+    }
+  }, [globalCart]);
+
+  const addressFormRender = () => {
+    return (
+      <div className={styles.formContainer}>
+        <h3 className={styles.title}>Shipping Address</h3>
+        <form className={styles.form} onSubmit={checkoutClickHandler}>
+          <label>Phone (ex: 01700000000)</label>
+          <Input type="text" placeholder="Enter your phone" variant="secondary" name="phone" required />
+          <label>Address</label>
+          <Input type="text" placeholder="Enter your address" variant="secondary" name="address" required />
+          <label>City</label>
+          <Input type="text" placeholder="Enter your city" variant="secondary" name="city" required />
+          <label>Country</label>
+          <Input type="text" placeholder="Enter your country" variant="secondary" name="country" required />
+          <br />
+          <div className={styles.checkContainer}>
+            <Input type="checkbox" name="check" className={styles.check} onChange={(e) => setCheckMark(e.target.checked)} />
+            <label className={styles.checkLabel}>
+              Please agree to our terms and conditions &nbsp;
+              <Link href="/terms-of-sale" className={styles.link}>
+                Terms and condition
+              </Link>
+            </label>
+          </div>
+          <Message text={message.text} type={message.type} />
+          <Button type="submit" variant="secondary" text="Checkout" loading={loading} className={styles.button} />
+        </form>
+      </div>
+    );
+  };
+  const coursePaymentInfoRender = () => {
+    return (
+      <div className={styles.infoWrapper}>
+        <h2 className={styles.title}>Course Details</h2>
+        <div className={styles.infoContainer}>
+          <h1 className={styles.title}>{globalCart?.courseTitle || ''}</h1>
+          {globalCart?.moduleTitle && (
+            <p className={styles.allModules}>
+              Module {globalCart?.moduleOrder}: {globalCart?.moduleTitle || ''}
+            </p>
+          )}
+          {globalCart?.allModules && (
+            <div className={styles.allModules}>
+              {globalCart?.allModules?.map((module) => (
+                <p key={module?._id}>
+                  Module {module?.order}: {module?.title || ''}
+                </p>
+              ))}
+            </div>
+          )}
+          <p className={styles.type}>
+            Type: <span>{globalCart?.type || ''}</span>{' '}
+          </p>
+          <p className={styles.price}>Price: {getAmountsWithCommas(globalCart?.price || globalCart?.modulePrice || '')}</p>
+        </div>
+        <br />
+        <h2 className={styles.title}>Payment </h2>
+        <div className={styles.paymentContainer}>
+          <p className={styles.paymentText}>Powered by</p>
+          <Image src="/assets/payment/1.png" alt="Empty" width={180} height={35} className={styles.paymentIcon} />
+        </div>
+      </div>
+    );
+  };
+  return (
+    <div className={styles.overlay}>
+      <div className={styles.header}>
+        <h3 className={styles.title}>Checkout</h3>
+      </div>
+      <div className={styles.main}>
+        <div className={styles.left}>{addressFormRender()}</div>
+        <div className={styles.right}>{coursePaymentInfoRender()}</div>
+      </div>
+    </div>
+  );
 }
