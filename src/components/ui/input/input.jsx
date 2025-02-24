e8eceacc5e36a06773a5a77b804968bba3967722
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-24 16:42:55 +0600

    bug fixes

--- src/components/ui/input/input.jsx
+++ src/components/ui/input/input.jsx
@@ -2,46 +2,44 @@
 import React from 'react';
 import styles from './input.module.css';
 export default function Input({
-	type = 'text',
-	placeholder = 'Enter your text',
-	value,
-	onChange = () => {},
-	name = '',
-	required = false,
-	disabled = false,
-	style = {},
-	variant = 'primary',
-	defaultValue,
-	className = '',
-	minLength,
-	maxLength,
-	pattern,
-	...props
+  type = 'text',
+  placeholder = 'Enter your text',
+  value,
+  onChange = () => {},
+  name = '',
+  required = false,
+  disabled = false,
+  style = {},
+  variant = 'primary',
+  defaultValue,
+  className = '',
+  minLength,
+  maxLength,
+  pattern,
 }) {
-	// If no value is provided, the input will manage its own state internally
+  // If no value is provided, the input will manage its own state internally
   const isControlled = value !== undefined;
 
-	//variants - primary, secondary, outLined
-	const handleChange = (e) => {
+  //variants - primary, secondary, outLined
+  const handleChange = (e) => {
     onChange(e);
   };
 
-	return (
-		<input
-			style={style}
-			type={type}
-			placeholder={placeholder}
-			onChange={handleChange}
-			{...(isControlled ? { value } : {defaultValue})}
-			className={`${styles.input} ${styles[variant]} ${className}`}
-			name={name}
-			required={required}
-			disabled={disabled}
-			pattern={type === 'tel' ? '^(?:\\+88|88)?(01[3-9]\\d{8})$' : pattern}
-			minLength={minLength}
-			maxLength={maxLength}
-			autoComplete='on'
-			{...props}
-		/>
-	);
+  return (
+    <input
+      style={style}
+      type={type}
+      placeholder={placeholder}
+      onChange={handleChange}
+      {...(isControlled ? { value } : { defaultValue })}
+      className={`${styles.input} ${styles[variant]} ${className}`}
+      name={name}
+      required={required}
+      disabled={disabled}
+      pattern={type === 'tel' ? '^(?:\\+88|88)?(01[3-9]\\d{8})$' : pattern}
+      minLength={minLength}
+      maxLength={maxLength}
+      autoComplete="on"
+    />
+  );
 }
