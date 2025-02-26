31845867aa250cbc72ff6c18f5b76ef44a5beb58
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-27 00:04:30 +0600

    quiz in admin dashboard

--- src/components/admin/courses/lessons/Quiz/quizDetails.jsx
+++ src/components/admin/courses/lessons/Quiz/quizDetails.jsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import styles from './quizDetails.module.css';
 import useAxios from '@/hooks/useAxios';
 import { configuration } from '@/configuration/configuration';
+import { FaCheckCircle } from 'react-icons/fa';
+
 export default function QuizDetails({ courseId, moduleId, lessonId }) {
   const [response, error, loading, axiosFetch] = useAxios();
   useEffect(() => {
@@ -11,6 +13,33 @@ export default function QuizDetails({ courseId, moduleId, lessonId }) {
       url: `${configuration.courses}/${courseId}/modules/${moduleId}/lessons/${lessonId}/quiz`,
     });
   }, []);
-  console.log(response);
-  return <div>QuizDetails</div>;
+
+  return (
+    <div className={styles.main}>
+      {response?.data?.quiz?.questions.map((question, index) => {
+        return (
+          <div key={index} className={styles.questionsContainer}>
+            <div className={styles.questionText}>
+              {index + 1}. {question?.question}
+            </div>
+            <div className={styles.optionsContainer}>
+              {question?.options.map((option, index) => {
+                return (
+                  <div key={index} className={styles.optionTextContainer}>
+                    <p className={ styles.optionText }>{index + 1}. {option?.option}</p>
+
+                    {option?.isCorrect && (
+                      <span className={styles.correct}>
+                        <FaCheckCircle className={styles.icon} />
+                      </span>
+                    )}
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
