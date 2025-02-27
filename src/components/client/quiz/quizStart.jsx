e50cd54bfaf003053592b29716b9b00ff2cca5b9
Shafin <100669577+shafin003@users.noreply.github.com>
2025-02-27 17:16:17 +0600

    bug fixes

--- src/components/client/quiz/quizStart.jsx
+++ src/components/client/quiz/quizStart.jsx
@@ -48,11 +48,18 @@ export default function QuizStart({ courseId, moduleId, lessonId }) {
         <div className={styles.quizDetails}>
           {checkEligibilityForQuizRender()}
           <p className={styles.title}>Title: {response?.data?.quiz?.title}</p>
-          <p>Maximum Attempts: {response?.data?.quiz?.maxAttempts}</p>
-          <p>Total Questions: {response?.data?.quiz?.questionCount}</p>
-          <p>Time Limit: {response?.data?.quiz?.quizTime} minutes</p>
-          <p>Total Marks: {response?.data?.quiz?.totalMarks}</p>
-          <p>Passing Score: {response?.data?.quiz?.passingScore}</p>
+          <p>
+            Maximum Attempts: {response?.data?.quiz?.maxAttempts}, Time: {response?.data?.quiz?.quizTime} minutes
+          </p>
+          <p>
+            Total Questions: {response?.data?.quiz?.questionCount}, Total Marks: {response?.data?.quiz?.totalMarks}, Passing
+            Score: {response?.data?.quiz?.passingScore}
+          </p>
+          <p className={styles.notes}>
+            Notes: Do not close or refresh the browser window while taking the quiz, as this may result in losing your progress.
+            Ensure a stable internet connection throughout the quiz to avoid interruptions. Avoid navigating away from this page
+            during the quiz to maintain the integrity of your answers.
+          </p>
           <div className={styles.buttonContainer}>
             <Button text="Back" variant="outLined" loading={loading} onClick={() => redirect(`/client/my-courses/${courseId}`)} />
             <Button text="Start Quiz" disabled={!response?.data?.canTakeQuiz} loading={loading} onClick={startQuizClickHAndler} />
