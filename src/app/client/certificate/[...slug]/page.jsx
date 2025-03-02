f0d84eac8c4aac0a3cb967788b45459d0988c37f
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-02 08:07:05 +0600

    bug fixes

--- src/app/client/certificate/[...slug]/page.jsx
+++ src/app/client/certificate/[...slug]/page.jsx
@@ -1,16 +1,18 @@
 'use client';
-import React, { use, useEffect } from 'react';
+import React, { use, useEffect, useState } from 'react';
 import styles from './page.module.css';
 import GenerateCertificate from '@/components/atom/generateCertificate';
 import useAxios from '@/hooks/useAxios';
 import { configuration } from '@/configuration/configuration';
 import LoadingDots from '@/components/ui/loading/loadingDots';
 import moment from 'moment';
+import Message from '@/components/ui/message/message';
 export default function Certificate({ params }) {
   const unwrappedParams = use(params);
   const courseId = unwrappedParams.slug[0];
   const moduleId = unwrappedParams.slug[1];
   const [response, error, loading, axiosFetch] = useAxios();
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     let url;
@@ -25,9 +27,18 @@ export default function Certificate({ params }) {
     });
   }, []);
 
+  useEffect(() => {
+    if (error?.message) {
+      setMessage({ text: error?.message, type: 'error' });
+    }
+  }, [error]);
+
   return (
     <div className={styles.main}>
       {loading && <LoadingDots />}
+      { message?.text && <div className={styles.msgContainer}>
+        <Message text={message?.text} type={message?.type} />
+      </div>}
       {response?.data?.studentName && !loading && !error && (
         <GenerateCertificate
           name={response?.data?.studentName}
