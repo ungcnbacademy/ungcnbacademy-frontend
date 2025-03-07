c8d18a86abcbca3e851af65259086499908cb689
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-07 13:58:49 +0600

    single quiz

---
+++ src/app/admin/quiz-review/[id]/page.jsx
@@ -0,0 +1,27 @@
+'use client';
+import React, { use, useEffect } from 'react';
+import styles from './page.module.css';
+import AllReview from '@/components/admin/quiz-review/allReview';
+import useAxios from '@/hooks/useAxios';
+import { configuration } from '@/configuration/configuration';
+export default function QuizReviewPage({ params }) {
+  const unwrappedParams = use(params);
+  const quizId = unwrappedParams.id;
+  const [responseGetQuizInfo, errorGetQuizInfo, loadingGetQuizInfo, axiosFetchGetQuizInfo] = useAxios();
+
+  useEffect(() => {
+    axiosFetchGetQuizInfo({
+      method: 'Get',
+      url: `${configuration.singleQuiz}/${quizId}`,
+    });
+  }, [quizId]);
+
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Quiz Review</h2>
+      </div>
+      <div className={styles.container}></div>
+    </div>
+  );
+}
--- src/app/admin/quiz-review/page.jsx
+++ src/app/admin/quiz-review/page.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import styles from './page.module.css';
 import AllReview from '@/components/admin/quiz-review/allReview';
 export default function QuizReviewPage() {
-  return <div className={styles.main}>
-  <div className={styles.header}>
-    <h2 className={styles.title}>Quiz Review</h2>
-  </div>
-  <div className={styles.container}>
-    <AllReview/>
-  </div>
-</div>;
+  return (
+    <div className={styles.main}>
+      <div className={styles.header}>
+        <h2 className={styles.title}>Quiz Review</h2>
+      </div>
+      <div className={styles.container}>
+        <AllReview />
+      </div>
+    </div>
+  );
 }
