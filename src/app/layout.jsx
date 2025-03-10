4f59fe637a470b9b1d381bfec2f8e479adf1fed8
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-11 02:49:00 +0600

    font added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,20 @@
 import CartContextProvider from '@/context/cartContext';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { futuraPt } from './font';
+
 const inter = Inter({ subsets: ['latin'] });
 export const metadata = {
-	title: 'ESG Education',
-	description: 'Find the best ESG education resources',
+  title: 'ESG Education',
+  description: 'Find the best ESG education resources',
 };
 
 export default function RootLayout({ children }) {
-	return (
-		<html lang="en">
-			<body className={inter.className}>
-				<CartContextProvider>{children}</CartContextProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body className={`${inter.className} ${futuraPt.variable}`}>
+        <CartContextProvider>{children}</CartContextProvider>
+      </body>
+    </html>
+  );
 }
