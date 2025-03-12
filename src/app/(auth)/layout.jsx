1d4d50958636461c9f18eb29a8101791f895760c
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-13 00:54:36 +0600

    auth multiple pic added

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,13 +1,24 @@
+'use client';
 import styles from './layout.module.css';
 import Footer from '@/components/footer/footer';
 import Navbar from '@/components/navbar/navbar';
+import { usePathname } from 'next/navigation';
 
 export default function Layout({ children }) {
+  const pathname = usePathname();
+  let backgroundImageUrl = '/assets/auth-bg.webp';
+
+  if (pathname === '/signup') {
+    backgroundImageUrl = '/assets/signup-bg.webp';
+  } else if (pathname === '/passwordReset') {
+    backgroundImageUrl = '/assets/passwordReset-bg.webp';
+  }
+
   return (
     <>
       <Navbar variant="transparentBlack" />
       <div className={styles.main}>
-        <div className={styles.left}></div>
+        <div className={styles.left} style={{ backgroundImage: `url(${backgroundImageUrl})` }}></div>
         <div className={styles.right}>{children}</div>
       </div>
       <Footer />
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -35,8 +35,8 @@ export const companyInfo = {
   websiteName: 'UNGCNB Academy',
   name: 'UNGCNB Academy',
   address: '6th Floor, BIMS Bhaban, 3 Progoti Sharani, Dhaka 1212, Bangladesh',
-  phone: '+8801626-658248',
-  email: 'info@esginstitutebd.com',
+  phone: '+8801671978757',
+  email: 'info@ungcnbacademy.com',
   website: 'https://esgeducation.netlify.app',
   googleMapAddress:
     'https://www.google.com/maps/place/ESG+Institute+Bangladesh/@23.7907918,90.4251343,17z/data=!3m1!4b1!4m6!3m5!1s0x3755c700057bae11:0xb1dc187232bc98b6!8m2!3d23.7907918!4d90.4251343!16s%2Fg%2F11ln9dw9zl?entry=ttu&g_ep=EgoyMDI1MDEyMi4wIKXMDSoASAFQAw%3D%3D',
