6434b330eaf28149fe78272519b50fccf6846ab7
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-13 08:19:41 +0600

    favicon added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import styles from './page.module.css';
 import Landing from '@/components/landing/landing';
-
+export const metadata = { title: 'Home | UNGCNB Academy' };
 export default function Home() {
-	return (
-		<div className={styles.main}>
-			<Landing />
-		</div>
-	);
+  return (
+    <div className={styles.main}>
+      <Landing />
+    </div>
+  );
 }
