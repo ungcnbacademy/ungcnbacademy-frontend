2f8e1ccd5324a1232570d2b30ab5a1214ce6f1ec
Shafin <100669577+shafin003@users.noreply.github.com>
2025-03-13 08:26:28 +0600

    bug fixes

--- src/app/(legal)/layout.jsx
+++ src/app/(legal)/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from '@/components/navbar/navbar';
 export default function Layout({ children }) {
 	return (
 		<>
-			<Navbar variant="white" />
+			<Navbar variant="transparent" />
 			{children}
 			<Footer />
 		</>
