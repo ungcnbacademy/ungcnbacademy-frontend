c899394c2ef6a2328eab6445bdcef1dc3995ecaa
Shafin <100669577+shafin003@users.noreply.github.com>
2025-04-28 17:18:28 +0600

    quiz result design bug fixes

--- src/components/client/quiz/quizResult.jsx
+++ src/components/client/quiz/quizResult.jsx
@@ -5,8 +5,11 @@ export default function QuizResult({ data }) {
     <>
       {data && (
         <div className={styles.main}>
+          <h3 className={styles.title}>Quiz result</h3>
           {data?.needsManualGrading && <p>Needs Manual Grading: {data?.needsManualGrading ? 'Yes' : 'No'}</p>}
-          <p>Passed: {data?.passed ? 'Yes' : 'No'}</p>
+          <p className={styles.score}>
+            Passed: {data?.passed ? <label className={styles.passed}>Yes</label> : <label className={styles.failed}>No</label>}
+          </p>
           <p>Score: {data?.score}</p>
           <p>Percentage: {data?.percentage}</p>
         </div>
